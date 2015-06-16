8665ffb6f2cd556e70fdced02d1c06b622e970ec
ozawaseijiro <seijiro4751@gmail.com>
2015-06-16 17:26:55 +0900

    add home controller

---
+++ api/controllers/HomeController.js
@@ -0,0 +1,11 @@
+/**
+ * HomeController
+ *
+ * @description :: Server-side logic for managing Homes
+ * @help        :: See http://links.sailsjs.org/docs/controllers
+ */
+
+module.exports = {
+	
+};
+
